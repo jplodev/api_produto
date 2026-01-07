74a40dc737a800c82b8237632393d636bac88dc3
João Paulo Lima <jplopapa.dev@gmail.com>
2026-01-07 19:50:28 -0300

    Commit inicial

---
+++ src/test/java/com/jpdev/apiproduto/ApiProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpdev.apiproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
